64aacdafd541212777e9395d377489c8ce577c69
Vamshi Itharaju <vamshiitharaju13@gmail.com>
2026-02-24 20:35:54 +0530

    Added prime number in a range program

---
+++ src/ProblemsNoNumbers/primeInRange.java
@@ -0,0 +1,30 @@
+package ProblemsNoNumbers;
+// prime numbers in a given range 
+import java.util.Scanner;
+public class primeInRange {
+	public static void main(String[]args)
+	{
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter Range");
+		int range=sc.nextInt();
+		for(int i=2;i<=range;i++)
+		{    
+			
+			int count=0;
+			for(int j=1;j<=i;j++)
+			{
+				if(i%j==0)
+				{
+					count++;
+				}
+			}
+			if(count==2)
+			{
+				System.out.print(i +",");
+			}
+		}
+		
+	}
+
+}
+
